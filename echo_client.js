dac216f260e0e50094331340bb8eba0f8a4f3413
frostcit2020 <frost.cit.2020@gmail.com>
2024-04-01 17:41:34 +0700

    Add client

---
+++ echo_client.js
@@ -0,0 +1,55 @@
+
+
+
+/** @type {WebSocket} */
+let WebSocketClient;
+if(typeof window === "object"){ // Browser runtime
+  WebSocketClient = WebSocket;
+}else{ // NodeJS runtime
+  WebSocketClient = require("ws")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//#region    MAIN
+async function main(){
+
+  /** @type {WebSocket} */
+  let connection;
+  try {
+    connection = new WebSocketClient('ws://localhost:8080');
+  } catch (error) {
+    console.error("Create connection error : " + error);
+    return;
+  }
+
+  connection.onopen = ()=>{
+    console.log("Connected");
+    connection.send("Hello from client");
+  };
+  connection.onerror = (err)=>{
+    console.error("error : " + err);
+  };
+  connection.onmessage = (event)=>{
+    console.log("message from server : " + event.data);
+  };
+  connection.onclose = (event)=>{
+    console.log("connection closed : " +  event);
+  }
+
+}
+main();
+//#endregion MAIN
+
+
+
